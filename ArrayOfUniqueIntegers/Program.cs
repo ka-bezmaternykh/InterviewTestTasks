7eaabe617f3e337ec67a43d8ae42537c5b823086
Kirill Bezmaternykh <ka.bezmaternykh@gmail.com>
2019-05-07 16:51:01 +0300

    Added ArrayOfUniqueIntegers

---
+++ ArrayOfUniqueIntegers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfUniqueIntegers
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Debugging
+            var array = PrimitiveAlgorithm(100000);
+        }
+
+        private static Random _random = new Random();
+
+        public static int[] PrimitiveAlgorithm(int numberOfIntegers = 100000)
+        {
+            var array = new int[numberOfIntegers];
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+                array[i] = i + 1;
+            }
+
+            var swapValue = 0;
+            var swapIndex = 0;
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+                swapIndex = _random.Next(1, numberOfIntegers);
+
+                swapValue = array[i];
+                array[i] = array[swapIndex];
+                array[swapIndex] = swapValue;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ ArrayOfUniqueIntegers/README.md
@@ -0,0 +1,10 @@
+**Introduction** \
+Greeting!
+
+**Description of the task** \
+Array of unique integers \
+Create an array with unique numbers 1 - 100 000 in random order in it. \
+Clarification: \
+There should be a single (1) array created. \
+The single array should have 100 000 elements in it. Each element should be an integer between 1 to 100 000. Each integer should be there only once (e.g. they should be unique). \
+You don’t need to print out the array. I can just check it and the code using debugger.
